1f516479f6d296990ab5d5537eac8b452ed6da6c
JFreshek <48886204+JFreshek@users.noreply.github.com>
2019-11-22 14:38:10 +0300

    Add UserRepository

---
+++ Code/src/main/java/com/yuditsky/advertsik/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.yuditsky.advertsik.repository;
+
+import com.yuditsky.advertsik.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
