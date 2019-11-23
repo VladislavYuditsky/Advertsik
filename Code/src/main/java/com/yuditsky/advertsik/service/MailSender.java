1c9358e1ec588af2bb95b9a153ddf880cf5c5b18
JFreshek <48886204+JFreshek@users.noreply.github.com>
2019-11-24 00:57:25 +0300

    Add Async

--- Code/src/main/java/com/yuditsky/advertsik/service/MailSender.java
+++ Code/src/main/java/com/yuditsky/advertsik/service/MailSender.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,6 +15,7 @@ public class MailSender {
     @Value("${spring.mail.username}")
     private String username;
 
+    @Async
     public void send(String emailTo, String subject, String message){
         SimpleMailMessage mailMessage = new SimpleMailMessage();
 
