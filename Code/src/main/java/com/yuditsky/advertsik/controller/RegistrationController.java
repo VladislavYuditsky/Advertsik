c382f59b49d85c8886177adae479e52ebb65f8c4
JFreshek <48886204+JFreshek@users.noreply.github.com>
2019-11-24 17:48:26 +0300

    Confirmation password validation changed

--- Code/src/main/java/com/yuditsky/advertsik/controller/RegistrationController.java
+++ Code/src/main/java/com/yuditsky/advertsik/controller/RegistrationController.java
@@ -5,10 +5,12 @@ import com.yuditsky.advertsik.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
 import java.util.Map;
@@ -24,12 +26,21 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String addUser(@Valid User user, BindingResult bindingResult, Model model){
-        if(user.getPassword() != null && !user.getPassword().equals(user.getConfirmationPassword())){
+    public String addUser(
+            @RequestParam("confirmationPassword") String confirmationPassword,
+            @Valid User user,
+            BindingResult bindingResult,
+            Model model){
+        boolean isConfirmEmpty = StringUtils.isEmpty(confirmationPassword);
+        if(isConfirmEmpty){
+            model.addAttribute("confirmationPasswordError", "Confirmation password can't be empty");
+        }
+
+        if(user.getPassword() != null && !user.getPassword().equals(confirmationPassword)){
             model.addAttribute("passwordError", "Password are different!");
         }
 
-        if(bindingResult.hasErrors()){
+        if(isConfirmEmpty || bindingResult.hasErrors()){
             Map<String, String> errors = ControllerUtil.getErrors(bindingResult);
 
             model.mergeAttributes(errors);
